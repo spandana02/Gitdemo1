219d99368a9090bf3a4889eb74fc97cd3d2090ea
spandana02 <dasarispandana2@gmail.com>
2021-05-08 20:01:55 +0530

    final commit

---
+++ src/test/java/TestMethods/Demo.java
@@ -0,0 +1,5 @@
+package TestMethods;
+
+public class Demo {
+
+}
--- src/test/java/TestMethods/Gittest.java
+++ src/test/java/TestMethods/Gittest.java
@@ -10,12 +10,16 @@ public class Gittest {
 		System.out.println("Second method created");
 	}
 
+	public void getData2() {
+		System.out.println("Gitx Change");
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		Gittest Git = new Gittest();
 		Git.getData();
 		Git.getData1();
+		Git.getData2();
 
 		System.out.println("Hello World");
 		System.out.println("Hey There!");
