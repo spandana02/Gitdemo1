b21c55eefcdb42e3ceff28153f0ac411dea6d79b
spandana02 <dasarispandana2@gmail.com>
2021-05-08 18:55:13 +0530

    Architech modified commit

--- src/test/java/TestMethods/Gittest.java
+++ src/test/java/TestMethods/Gittest.java
@@ -2,8 +2,19 @@ package TestMethods;
 
 public class Gittest {
 
+	public void getData() {
+		System.out.println("New method created");
+	}
+	
+	public void getData1() {
+		System.out.println("Second method created");
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		Gittest Git = new Gittest();
+		Git.getData();
 
 		System.out.println("Hello World");
 		System.out.println("Hey There!");
